a71d8a0a8381294fa3b4156cd778c68ed2b28534
WannaBAcoder <vinnie.marco95@gmail.com>
2024-10-16 21:10:54 -0500

    added ramp, reverse direction protection, faster PWM

--- DMXStageController/DMXStageController/DMXStageController.ino
+++ DMXStageController/DMXStageController/DMXStageController.ino
@@ -4,68 +4,87 @@
 #define DIR_CHANNEL 1
 #define SPEED_CHANNEL 2
 
-#define MOTOR1_A 7
-#define MOTOR1_B 8
-#define MOTOR2_A 9
-#define MOTOR2_B 10
+// Define PWM pins based on Timer 1 (pins 11, 12) and Timer 3 (pins 2, 3, 5)
+#define MOTOR1_A 11 // Pin tied to Timer 1
+#define MOTOR1_B 12 // Pin tied to Timer 1
+#define MOTOR2_A 2  // Pin tied to Timer 3
+#define MOTOR2_B 3  // Pin tied to Timer 3
 
 // Configure a DMX slave controller
 DMX_Slave dmx_slave ( DMX_SLAVE_CHANNELS );
 
-const int ledPin = 13;
+const int ledPin = 13; // For debugging motor speed
 
-unsigned long       lastFrameReceivedTime;
+unsigned long lastFrameReceivedTime;
 const unsigned long dmxTimeoutMillis = 5000UL;
 
 bool motor_dir = 0;
+bool target_dir = 0;    // Store the target direction
 uint8_t motor_speed = 0;
+uint8_t current_speed = 0; // Current speed of the motor
+uint8_t target_speed = 0;  // Target speed set by DMX controller
+
+const unsigned long ramp_interval = 100; // Interval in ms to adjust the speed
+unsigned long last_ramp_time = 0;
+const uint8_t ramp_step = 5; // Amount to increase/decrease speed per interval
 
 // the setup routine runs once when you press reset:
 void setup() {             
-  
   // Enable DMX slave interface and start recording
   dmx_slave.enable();  
-  
-  // Set start address to 1, this is also the default setting
-  // You can change this address at any time during the program
   dmx_slave.setStartAddress(1);
+  dmx_slave.onReceiveComplete(OnFrameReceiveComplete);
   
-  //
-  // Register on frame complete event to determine signal timeout
-  //
-  dmx_slave.onReceiveComplete ( OnFrameReceiveComplete );
-  
-  // Set led pin as output pin
+  // Set up pins
   pinMode(ledPin, OUTPUT );
   pinMode(MOTOR1_A, OUTPUT);
   pinMode(MOTOR1_B, OUTPUT);
   pinMode(MOTOR2_A, OUTPUT);
   pinMode(MOTOR2_B, OUTPUT);
+  
+  //Setup timers for PWM with higher frequency
+  setupTimer1ForPWM();
+  setupTimer3ForPWM();
 }
 
 // the loop routine runs over and over again forever:
 void loop() 
 {
-   // Get current time
    unsigned long now = millis();
  
-   // If we didn't receive a DMX frame within the timeout period 
-   // clear all dmx channels
-   if(now - lastFrameReceivedTime > dmxTimeoutMillis )
+   // If no DMX frame within the timeout period, clear all DMX channels
+   if(now - lastFrameReceivedTime > dmxTimeoutMillis)
        dmx_slave.getBuffer().clear();
 
-  //set direction of motor
-  if (dmx_slave.getChannelValue(DIR_CHANNEL) > 127 )
-    motor_dir = 1;
-  else
-    motor_dir = 0;
+   // Read target direction and speed from DMX
+   target_dir = (dmx_slave.getChannelValue(DIR_CHANNEL) > 127) ? 1 : 0;
+   target_speed = dmx_slave.getChannelValue(SPEED_CHANNEL);
 
-  //get motor speed
-  motor_speed = dmx_slave.getChannelValue(SPEED_CHANNEL);
-  analogWrite(ledPin, motor_speed);
+   // Handle speed ramping every ramp_interval milliseconds
+   if (now - last_ramp_time >= ramp_interval) {
+       adjustSpeed(); // Function to ramp up/down the speed
+       last_ramp_time = now;
+   }
 
-  drive_motors();
-    
+   drive_motors();    
+}
+
+void adjustSpeed()
+{
+  // If the current speed is not at the target, adjust incrementally
+  if (current_speed < target_speed) {
+    current_speed = min(current_speed + ramp_step, target_speed); // Ramp up
+  }
+  else if (current_speed > target_speed) {
+    current_speed = max(current_speed - ramp_step, target_speed); // Ramp down
+  }
+
+  // Only allow direction change if current speed is zero
+  if (current_speed == 0) {
+    motor_dir = target_dir;
+  }
+
+  analogWrite(ledPin, current_speed); // Update LED with current speed
 }
 
 void drive_motors(void)
@@ -73,37 +92,54 @@ void drive_motors(void)
   if(motor_dir)
   {
     analogWrite(MOTOR1_A, 0);
-    analogWrite(MOTOR1_B, motor_speed);
+    analogWrite(MOTOR1_B, current_speed);
 
     analogWrite(MOTOR2_A, 0);
-    analogWrite(MOTOR2_B, motor_speed);
+    analogWrite(MOTOR2_B, current_speed);
   }
-
   else
   {
     analogWrite(MOTOR1_B, 0);
-    analogWrite(MOTOR1_A, motor_speed);
+    analogWrite(MOTOR1_A, current_speed);
 
     analogWrite(MOTOR2_B, 0);
-    analogWrite(MOTOR2_A, motor_speed);
+    analogWrite(MOTOR2_A, current_speed);
   }
+}
 
-  return;
+// Set up Timer 1 (pins 11 and 12) for 20 kHz PWM
+void setupTimer1ForPWM() {
+  // Stop Timer 1
+  TCCR1B = 0;  
+
+  // Set the mode to Fast PWM (mode 14)
+  TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11);
+  TCCR1B = _BV(WGM13) | _BV(WGM12);
+
+  // Set prescaler to 8 (for higher frequency PWM)
+  TCCR1B |= _BV(CS11);
+
+  // Set the top value (ICR1) to achieve 20 kHz frequency
+  ICR1 = 99;
+}
+
+// Set up Timer 3 (pins 2, 3, and 5) for 20 kHz PWM
+void setupTimer3ForPWM() {
+  // Stop Timer 3
+  TCCR3B = 0;  
+
+  // Set the mode to Fast PWM (mode 14)
+  TCCR3A = _BV(COM3A1) | _BV(COM3B1) | _BV(WGM31);
+  TCCR3B = _BV(WGM33) | _BV(WGM32);
+
+  // Set prescaler to 8 (for higher frequency PWM)
+  TCCR3B |= _BV(CS31);
+
+  // Set the top value (ICR3) to achieve 20 kHz frequency
+  ICR3 = 99;
 }
 
 void OnFrameReceiveComplete (unsigned short channelsReceived)
 {
-  if (channelsReceived == DMX_SLAVE_CHANNELS)
-  {
-    // All slave channels have been received
-  }
-  else
-  {
-    // We have received a frame but not all channels we where 
-    // waiting for, master might have transmitted less
-    // channels
-  }
-
-  // Update receive time to determine signal timeout
   lastFrameReceivedTime = millis ();
 }
---
+++ DMXStageController/DMXStageController_simple/DMXStageController_simple.ino
@@ -0,0 +1,109 @@
+#include <Conceptinetics.h>
+#define DMX_SLAVE_CHANNELS   2 
+
+#define DIR_CHANNEL 1
+#define SPEED_CHANNEL 2
+
+#define MOTOR1_A 7
+#define MOTOR1_B 8
+#define MOTOR2_A 9
+#define MOTOR2_B 10
+
+// Configure a DMX slave controller
+DMX_Slave dmx_slave ( DMX_SLAVE_CHANNELS );
+
+const int ledPin = 13;
+
+unsigned long       lastFrameReceivedTime;
+const unsigned long dmxTimeoutMillis = 5000UL;
+
+bool motor_dir = 0;
+uint8_t motor_speed = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {             
+  
+  // Enable DMX slave interface and start recording
+  dmx_slave.enable();  
+  
+  // Set start address to 1, this is also the default setting
+  // You can change this address at any time during the program
+  dmx_slave.setStartAddress(1);
+  
+  //
+  // Register on frame complete event to determine signal timeout
+  //
+  dmx_slave.onReceiveComplete(OnFrameReceiveComplete);
+  
+  //Set up pins
+  pinMode(ledPin, OUTPUT );
+  pinMode(MOTOR1_A, OUTPUT);
+  pinMode(MOTOR1_B, OUTPUT);
+  pinMode(MOTOR2_A, OUTPUT);
+  pinMode(MOTOR2_B, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() 
+{
+   // Get current time
+   unsigned long now = millis();
+ 
+   // If we didn't receive a DMX frame within the timeout period 
+   // clear all dmx channels
+   if(now - lastFrameReceivedTime > dmxTimeoutMillis)
+       dmx_slave.getBuffer().clear();
+
+  //set direction of motor
+  if (dmx_slave.getChannelValue(DIR_CHANNEL) > 127)
+    motor_dir = 1;
+  else
+    motor_dir = 0;
+
+  //get motor speed
+  motor_speed = dmx_slave.getChannelValue(SPEED_CHANNEL);
+  analogWrite(ledPin, motor_speed);
+
+  drive_motors();
+    
+}
+
+void drive_motors(void)
+{
+  if(motor_dir)
+  {
+    analogWrite(MOTOR1_A, 0);
+    analogWrite(MOTOR1_B, motor_speed);
+
+    analogWrite(MOTOR2_A, 0);
+    analogWrite(MOTOR2_B, motor_speed);
+  }
+
+  else
+  {
+    analogWrite(MOTOR1_B, 0);
+    analogWrite(MOTOR1_A, motor_speed);
+
+    analogWrite(MOTOR2_B, 0);
+    analogWrite(MOTOR2_A, motor_speed);
+  }
+
+  return;
+}
+
+void OnFrameReceiveComplete (unsigned short channelsReceived)
+{
+//  if (channelsReceived == DMX_SLAVE_CHANNELS)
+//  {
+//    // All slave channels have been received
+//  }
+//  else
+//  {
+//    // We have received a frame but not all channels we where 
+//    // waiting for, master might have transmitted less
+//    // channels
+//  }
+
+  // Update receive time to determine signal timeout
+  lastFrameReceivedTime = millis ();
+}
